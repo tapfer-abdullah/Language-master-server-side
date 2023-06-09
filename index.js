a317993c97263559bdcf8ee32c29e372b70f3105
tapfer-abdullah <tapfer.abdullah@gmail.com>
2023-06-09 14:32:10 +0600

    all courses data load from mongodb

--- index.js
+++ index.js
@@ -45,6 +45,28 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("LanguageMasterDB");
+    const LMInstructors = database.collection("LMInstructors");
+    const LMCourses = database.collection("LMCourses");
+
+
+    app.get("/instructors", async(req, res)=>{
+      const query = {};
+        const cursor = LMInstructors.find(query);
+        const instructors = await cursor.toArray();
+        res.send(instructors);
+    })
+
+    app.get("/courses", async(req, res)=>{
+      const query = {};
+        const cursor = LMCourses.find(query);
+        const courses = await cursor.toArray();
+        res.send(courses);
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
