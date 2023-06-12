6de14409ab5a3f790b0d1cad3729974d7865663d
tapfer-abdullah <tapfer.abdullah@gmail.com>
2023-06-12 20:53:48 +0600

    jwt added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-
+const jwt = require('jsonwebtoken');
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -19,7 +19,25 @@ app.use(cors(corsConfig));
 // app.use(cors());
 app.use(express.json());
 
-const language = require("./language.json")
+// const language = require("./language.json")
+
+const verifyJWT = (req, res, next)=>{
+  const authorization = req.headers.authorization;
+
+  if(!authorization){
+    return res.status(401).send({error: true, message: "Unauthorized access"});
+  }
+
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.LM_ACCESS_SECRET, function(err, decoded) {
+    if(err){
+      return res.status(401).send({error: true, message: "Unauthorized access"});
+    }
+
+    req.decoded = decoded;
+    next();
+  });
+}
 
 
 
@@ -52,36 +70,50 @@ async function run() {
     const LMCourses = database.collection("LMCourses");
     const LMUserCarts = database.collection("LMUserCarts");
 
+    // JWT 
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.LM_ACCESS_SECRET, { expiresIn: '1h' });
+      res.send(token);
+    })
 
     // user and instructors  regular route 
     app.get("/instructors", async (req, res) => {
-      const query = {designation: "Instructor"};
+      const query = { designation: "Instructor" };
       const cursor = LMInstructors.find(query);
       const instructors = await cursor.toArray();
       res.send(instructors);
     })
 
-    app.post("/user", async(req, res)=>{
+    app.post("/user", async (req, res) => {
       const user = req.body;
-      const query = {email: user.email}
+      const query = { email: user.email }
 
       const existingUser = await LMInstructors.findOne(query);
 
-      if(existingUser){
-        return res.send({message: "User already exist!"});
+      if (existingUser) {
+        return res.send({ message: "User already exist!" });
       }
 
       const result = await LMInstructors.insertOne(user);
       console.log(user)
       res.send(result)
-      
+
     })
 
     app.get("/courses", async (req, res) => {
-      const query = {status: "approved"};
-      const cursor = LMCourses.find(query);
-      const courses = await cursor.toArray();
-      res.send(courses);
+
+      if (req?.query?.sort) {
+        const cursor = LMCourses.find().sort({ enrolled: req.query.sort })
+        const courses = await cursor.toArray();
+        res.send(courses);
+      }
+      else {
+        const query = { status: "approved" };
+        const cursor = LMCourses.find(query);
+        const courses = await cursor.toArray();
+        res.send(courses);
+      }
     })
 
     //select to my cart 
@@ -101,10 +133,14 @@ async function run() {
       res.send(result)
     })
 
-    app.get("/my-selected-course", async (req, res) => {
+    app.get("/my-selected-course", verifyJWT, async (req, res) => {
       let query = req.query || {};
 
-      console.log(query);
+      // console.log(req.decoded.loggedUser.email);
+      const decodedEmail = req?.decoded?.loggedUser?.email;
+      if(query?.email != decodedEmail){
+        return res.status(403).send({error: true, message: "Forbidden access"})
+      }
 
       if (query) {
         query = { status: query.status, email: query.email };
@@ -128,22 +164,22 @@ async function run() {
     })
 
     // update role 
-    app.patch("/user/:email", async(req, res)=>{
+    app.patch("/user/:email", async (req, res) => {
       const email = req.params.email;
       const role = req.body.role;
       // console.log(email, role)
 
 
-      const filter = { email: email};
+      const filter = { email: email };
       const options = { upsert: true };
 
-    const updateDoc = {
-      $set: {
-        designation: role
-      },
-    };
-    const result = await LMInstructors.updateOne(filter, updateDoc, options);
-    res.send(result)
+      const updateDoc = {
+        $set: {
+          designation: role
+        },
+      };
+      const result = await LMInstructors.updateOne(filter, updateDoc, options);
+      res.send(result)
 
     })
 
@@ -157,27 +193,27 @@ async function run() {
     })
 
     // update status
-    app.patch("/class/:id", async(req, res)=>{
+    app.patch("/class/:id", async (req, res) => {
       const id = req.params.id;
       const status = req.body.status;
       console.log(id, status)
 
 
-      const filter = { _id: new ObjectId(id)};
+      const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
 
-    const updateDoc = {
-      $set: {
-        status: status
-      },
-    };
-    const result = await LMCourses.updateOne(filter, updateDoc, options);
-    res.send(result)
+      const updateDoc = {
+        $set: {
+          status: status
+        },
+      };
+      const result = await LMCourses.updateOne(filter, updateDoc, options);
+      res.send(result)
     })
 
     // Instructor options ------------
     // -------------------------------------
-    app.post("/add-class", async(req, res)=>{
+    app.post("/add-class", async (req, res) => {
       const newClass = req.body;
       console.log(newClass)
 
@@ -186,72 +222,72 @@ async function run() {
     })
 
     // single class data by id 
-    app.get("/class/:id", async(req, res)=>{
+    app.get("/class/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
 
-      const result= await LMCourses.findOne(query)
+      const result = await LMCourses.findOne(query)
       res.send(result);
       // console.log(id, result)
     })
 
     // update a class 
-    app.patch("/update-class/:id", async(req, res)=>{
+    app.patch("/update-class/:id", async (req, res) => {
       const id = req.params.id;
       const newClass = req.body;
-      console.log("new",newClass)
+      console.log("new", newClass)
 
 
-      const filter = { _id: new ObjectId(id)};
+      const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
 
-    const updateDoc = {
-      $set: {
-        image: newClass.image,
-        price: newClass.price,
-        name: newClass.name,
-        availableSeats: newClass.availableSeats
-      },
-    };
-    const result = await LMCourses.updateOne(filter, updateDoc, options);
-    res.send(result)
+      const updateDoc = {
+        $set: {
+          image: newClass.image,
+          price: newClass.price,
+          name: newClass.name,
+          availableSeats: newClass.availableSeats
+        },
+      };
+      const result = await LMCourses.updateOne(filter, updateDoc, options);
+      res.send(result)
     })
 
 
-    app.patch("/course/:id", async(req, res)=>{
+    app.patch("/course/:id", async (req, res) => {
       const id = req.params.id;
       const newClass = req.body;
-      console.log("new",newClass)
+      console.log("new", newClass)
 
 
-      const filter = { _id: new ObjectId(id)};
+      const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
 
-    const updateDoc = {
-      $set: {
-        enrolled: newClass.enrolled
-      },
-    };
-    const result = await LMCourses.updateOne(filter, updateDoc, options);
-    res.send(result)
+      const updateDoc = {
+        $set: {
+          enrolled: newClass.enrolled
+        },
+      };
+      const result = await LMCourses.updateOne(filter, updateDoc, options);
+      res.send(result)
     })
 
-    app.patch("/course-feedback/:id", async(req, res)=>{
+    app.patch("/course-feedback/:id", async (req, res) => {
       const id = req.params.id;
       const newClass = req.body;
-      console.log("new",newClass)
+      console.log("new", newClass)
 
 
-      const filter = { _id: new ObjectId(id)};
+      const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
 
-    const updateDoc = {
-      $set: {
-        feedback: newClass.feedback
-      },
-    };
-    const result = await LMCourses.updateOne(filter, updateDoc, options);
-    res.send(result)
+      const updateDoc = {
+        $set: {
+          feedback: newClass.feedback
+        },
+      };
+      const result = await LMCourses.updateOne(filter, updateDoc, options);
+      res.send(result)
     })
 
 
